8fb2af594f6bb9eeb1d984963ee35b05e061f6b1
Mark C <markbcoronel@yahoo.com>
2024-09-18 20:05:47 +0100

    Added project from spring initialzr

---
+++ src/main/java/com/flavourvault/flavour_vault_backend/FlavourVaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flavourvault.flavour_vault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourVaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavourVaultBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavourvault/flavour_vault_backend/FlavourVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavourvault.flavour_vault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavourVaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
