e5093e9f94cf2c2639a3e761ba5a755ec27c18fe
Mark C <markbcoronel@yahoo.com>
2024-09-19 00:07:22 +0100

    Added RecipeManagementService service class

--- src/main/java/com/flavourvault/flavour_vault_backend/service/RecipeManagementService.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/service/RecipeManagementService.java
@@ -1,5 +1,31 @@
 package com.flavourvault.flavour_vault_backend.service;
 
-public class RecipeManagementService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.flavourvault.flavour_vault_backend.model.Recipe;
+import com.flavourvault.flavour_vault_backend.repository.RecipeRepository;
+
+@Service
+public class RecipeManagementService {
+	
+	/**
+	 * @Autowired annotation injects the dependency. Tells spring to inject an instance of a class into another class.
+	 */
+	@Autowired
+	private RecipeRepository recipeRepository;
+	
+	
+	//Business Logic
+	
+	public Recipe createRecipe(Recipe recipe) {
+		return recipeRepository.save(recipe);
+	}
+	
+	public Recipe getRecipe(Long id) {
+		return recipeRepository.findById(id).orElse(null);
+	}
+	
+	
+	
 }
