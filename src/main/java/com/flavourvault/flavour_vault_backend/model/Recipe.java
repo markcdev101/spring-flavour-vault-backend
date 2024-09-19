06e35beb861bd06f4ce84bfdd9c80341dcee2b7f
Mark C <markbcoronel@yahoo.com>
2024-09-19 12:03:06 +0100

    Controller test fixed

--- src/main/java/com/flavourvault/flavour_vault_backend/model/Recipe.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/model/Recipe.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,7 +19,7 @@ import lombok.Setter;
  */
 @Entity
 @Table(name = "recipes")
-@Getter @Setter @NoArgsConstructor
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Recipe {
 	
 	@Id
