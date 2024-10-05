8982d433e0c42a98bb33a577385d846d37a778ab
Mark C <markbcoronel@yahoo.com>
2024-10-05 21:27:56 +0100

    JwtTokenProvider crearted

---
+++ src/main/java/com/flavourvault/flavour_vault_backend/security/JwtTokenProvider.java
@@ -0,0 +1,85 @@
+package com.flavourvault.flavour_vault_backend.security;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtTokenProvider {
+	
+	@Value("${app.jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${app.jwtExpirationMs}")
+	private int jwtExpirationMs;
+	
+	/**
+	 * Get Signing Key
+	 * @return
+	 */
+	private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+	
+	/**
+	 * Generate JWT Token
+	 * @param authentication
+	 * @return
+	 */
+	public String generateToken(Authentication authentication) {
+		String username = authentication.getName();
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + jwtExpirationMs);
+		
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(now)
+				.setExpiration(expiryDate)
+				.signWith(getSigningKey(), SignatureAlgorithm.HS256)
+				.compact();
+	}
+	
+	
+	/**
+	 * Validate JWT Token
+	 * @param token
+	 * @return
+	 */
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	/**
+	 * Get username from JWT Token
+	 * @param token
+	 * @return
+	 */
+	public String getUserNameFromToken(String token) {
+		Claims claims = Jwts.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		
+		return claims.getSubject();
+	}
+	
+
+	
+}
