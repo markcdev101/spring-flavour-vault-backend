d8f1afdf1a1a34748748baf9c2749016b92eccf9
Mark C <markbcoronel@yahoo.com>
2024-10-05 23:51:21 +0100

    Updated User JPA entity and implements with UserDetails

--- src/main/java/com/flavourvault/flavour_vault_backend/model/User.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/model/User.java
@@ -1,9 +1,13 @@
 package com.flavourvault.flavour_vault_backend.model;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,7 +25,7 @@ import lombok.Setter;
 @Table(name = "users")
 @Entity
 @Getter @Setter @AllArgsConstructor
-public class User {
+public class User implements UserDetails{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -44,5 +48,34 @@ public class User {
 	@UpdateTimestamp
 	@Column(name="updated_at")
 	private Date updatedAt;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of();
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
 	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled(){ 
+		return true;
+	} 
 }
