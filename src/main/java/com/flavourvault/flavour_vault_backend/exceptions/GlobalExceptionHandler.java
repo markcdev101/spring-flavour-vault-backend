011018a020a02a543f239fd20e9d65bf46277dd7
Mark C <markbcoronel@yahoo.com>
2024-10-06 17:50:32 +0100

    Added user controller service and globalexceptionhandler

--- src/main/java/com/flavourvault/flavour_vault_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.flavourvault.flavour_vault_backend.exceptions;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.access.AccessDeniedException;
@@ -10,6 +12,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
@@ -20,6 +23,7 @@ public class GlobalExceptionHandler {
         exception.printStackTrace();
 
         if (exception instanceof BadCredentialsException) {
+        	log.error("BadCredentialsException: {}", exception.getMessage());
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
             errorDetail.setProperty("description", "The username or password is incorrect");
 
@@ -27,21 +31,25 @@ public class GlobalExceptionHandler {
         }
 
         if (exception instanceof AccountStatusException) {
+        	log.error("AccountStatusException: {}", exception.getMessage());
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The account is locked");
         }
 
         if (exception instanceof AccessDeniedException) {
+        	log.error("AccessDeniedException: {}", exception.getMessage());
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "You are not authorized to access this resource");
         }
 
         if (exception instanceof SignatureException) {
+        	log.error("SignatureException: {}", exception.getMessage());
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The JWT signature is invalid");
         }
 
         if (exception instanceof ExpiredJwtException) {
+        	log.error("ExpiredJwtException: {}", exception.getMessage());
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The JWT token has expired");
         }
