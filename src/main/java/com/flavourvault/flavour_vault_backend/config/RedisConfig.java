b624fcf9d0ed047717f0d64825df477c6bf2a904
Mark C <markbcoronel@yahoo.com>
2024-10-20 15:55:59 +0100

    Fixed Redis Config

--- src/main/java/com/flavourvault/flavour_vault_backend/config/RedisConfig.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/config/RedisConfig.java
@@ -1,11 +1,13 @@
 package com.flavourvault.flavour_vault_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
@@ -15,20 +17,39 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @Configuration
 public class RedisConfig {
 
-	 @Bean
-	    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-	        RedisTemplate<String, Object> template = new RedisTemplate<>();
-	        template.setConnectionFactory(redisConnectionFactory);
+    // Load Redis properties from application.yaml
+    @Value("${spring.redis.host}")
+    private String redisHost;
 
-	        // Configure the serializer
-	        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-	        template.setValueSerializer(serializer);
-	        template.setKeySerializer(RedisSerializer.string());
-	        return template;
-	    }
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
-	    @Bean
-	    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-	        return RedisCacheManager.builder(redisConnectionFactory).build();
-	    }
+    @Value("${spring.redis.password:}") // Default to empty if no password is set
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        if (!redisPassword.isEmpty()) {
+            config.setPassword(redisPassword); // Set password only if it's not empty
+        }
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // Configure the serializer
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        template.setValueSerializer(serializer);
+        template.setKeySerializer(RedisSerializer.string());
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory).build();
+    }
 }
