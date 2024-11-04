171ea667dee9d3602420c576c9faedc1b3e83364
Mark C <markbcoronel@yahoo.com>
2024-11-04 14:53:03 +0000

    Entity added for profile

---
+++ src/main/java/com/flavourvault/flavour_vault_backend/entities/Profile.java
@@ -0,0 +1,62 @@
+package com.flavourvault.flavour_vault_backend.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * JPA Entity for User
+ */
+@Table(name = "profile")
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Profile implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 9089058303008421709L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+}
