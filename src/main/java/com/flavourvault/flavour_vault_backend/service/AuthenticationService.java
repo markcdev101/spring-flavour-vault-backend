44eb5e9cf2d84144908049777300c68088960e4c
Mark C <markbcoronel@yahoo.com>
2024-11-04 17:26:25 +0000

    Updated Login

--- src/main/java/com/flavourvault/flavour_vault_backend/service/AuthenticationService.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/service/AuthenticationService.java
@@ -71,7 +71,7 @@ public class AuthenticationService {
     public User authenticate(LoginUserDto input) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        input.getEmail(),
+                        input.getUsername(),
                         input.getPassword()
                 )
         );
