43fe068f54928ce991918628aaa73acabef201f7
Mark C <markbcoronel@yahoo.com>
2024-11-07 18:57:29 +0000

    Added Service and Update Repository for Kitchen Inventory

--- src/main/java/com/flavourvault/flavour_vault_backend/repository/KitchenInventoryRepository.java
+++ src/main/java/com/flavourvault/flavour_vault_backend/repository/KitchenInventoryRepository.java
@@ -1,5 +1,8 @@
 package com.flavourvault.flavour_vault_backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.flavourvault.flavour_vault_backend.entities.Kitchen_Inventory;
 
 @Repository
 public interface KitchenInventoryRepository extends JpaRepository<Kitchen_Inventory, Long> {
+	// Custom query method to find by barcode
+    Optional<Kitchen_Inventory> findByBarcode(String barcode);
 
+    // Custom query method to find by name (case-insensitive)
+    List<Kitchen_Inventory> findByNameIgnoreCase(String name);
 }
---
+++ src/main/java/com/flavourvault/flavour_vault_backend/service/KitchenInventoryManagementService.java
@@ -0,0 +1,57 @@
+package com.flavourvault.flavour_vault_backend.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flavourvault.flavour_vault_backend.entities.Kitchen_Inventory;
+import com.flavourvault.flavour_vault_backend.repository.KitchenInventoryRepository;
+
+@Service
+public class KitchenInventoryManagementService {
+	
+	@Autowired
+	private KitchenInventoryRepository kitchenInventoryRepository;
+	
+	
+	// Fetching kitchen inventory item by barcode
+	public Optional<Kitchen_Inventory> getInventoryByBarcode(String barcode) {
+	    return kitchenInventoryRepository.findByBarcode(barcode);
+	}
+
+	// Fetching kitchen inventory items by name
+	public List<Kitchen_Inventory> getInventoryByName(String name) {
+	    return kitchenInventoryRepository.findByNameIgnoreCase(name);
+	}
+	
+	// Updating a kitchen inventory item
+    public Kitchen_Inventory updateInventory(Long id, Kitchen_Inventory updatedInventory) {
+        Optional<Kitchen_Inventory> existingInventoryOptional = kitchenInventoryRepository.findById(id);
+
+        if (existingInventoryOptional.isPresent()) {
+            Kitchen_Inventory existingInventory = existingInventoryOptional.get();
+            existingInventory.setBrandName(updatedInventory.getBrandName());
+            existingInventory.setName(updatedInventory.getName());
+            existingInventory.setBarcode(updatedInventory.getBarcode());
+            existingInventory.setLocation(updatedInventory.getLocation());
+            existingInventory.setQuatity(updatedInventory.getQuatity());
+            existingInventory.setExpirationDate(updatedInventory.getExpirationDate());
+            existingInventory.setBestBeforeDate(updatedInventory.getBestBeforeDate());
+            existingInventory.setPrice(updatedInventory.getPrice());
+            return kitchenInventoryRepository.save(existingInventory);
+        } else {
+            throw new RuntimeException("Kitchen inventory item not found with id: " + id);
+        }
+    }
+
+    // Deleting a kitchen inventory item by ID
+    public void deleteInventory(Long id) {
+        if (kitchenInventoryRepository.existsById(id)) {
+            kitchenInventoryRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Kitchen inventory item not found with id: " + id);
+        }
+    }
+}
